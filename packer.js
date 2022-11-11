cb79bf4c59a68d9436043c3f8c554238614207d8
MarJose <18107626+MarJose123@users.noreply.github.com>
2022-11-11 09:57:26 +0800

    fix: Improper file

--- packer.js
+++ packer.js
@@ -1,21 +1,19 @@
 // eslint-disable-next-line no-undef
-const AdmZip = require('adm-zip');
+const AdmZip = require("adm-zip");
 // eslint-disable-next-line no-undef
-const fs = require('fs');
-
-const dist = './packed';
+const fs = require("fs");
 
 async function createZipArchive() {
-    const zip = new AdmZip();
+  const zip = new AdmZip();
 
-    if (!fs.existsSync(dist)) {
-        fs.mkdirSync(dist);
-    }
+  if (!fs.existsSync("./dist")) {
+    fs.mkdirSync("./dist");
+  }
 
-    const outputFile = './packed/index.zip';
-    zip.addLocalFolder('./dist');
-    zip.writeZip(outputFile);
-    console.log(`Created ${outputFile} successfully`);
+  const outputFile = "./dist/app.zip";
+  zip.addLocalFolder("./build");
+  zip.writeZip(outputFile);
+  console.log(`Created ${outputFile} successfully`);
 }
 
 createZipArchive();
--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 
 import "./assets/main.css";
-
+let ZOHO;
+ZOHO.embeddedApp.init();
 createApp(App).mount("#app");
